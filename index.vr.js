32f55f35ea96587ee0536f83230aef8a5d85aa51
Andrew Kenny <andrew.kenny@t4g.com>
2018-01-23 18:35:45 -0400

    Added proper animations using the Animated API provided by React VR. TODO: Figure out how to make animations more modular. Right now each model has seperate animations that do similar things.

--- index.vr.js
+++ index.vr.js
@@ -6,9 +6,11 @@ import {
     Text,
     View,
     Model,
-
+    Animated,
 } from 'react-vr';
 
+
+const AnimatedModel = Animated.createAnimatedComponent(Model);
 
 class CreatureInfo extends React.Component{
 
@@ -36,7 +38,7 @@ class InfoBox extends React.Component{
     renderCreatureInfo(c, id){
         //console.log("Rendered " + c);
         let creature = this.props.creatures[c];
-        return <CreatureInfo id={id} name={creature.name} sound={creature.sound}/>
+        return <CreatureInfo key={id} name={creature.name} sound={creature.sound}/>
     }
 
     render(){
@@ -57,6 +59,9 @@ class InfoBox extends React.Component{
 }
 
 class RabbitChase extends React.Component {
+
+    /*CONSTRUCTOR FOR ENTIRE SCENE*/
+
     constructor(props){
         super(props);
         this.state = {
@@ -64,78 +69,152 @@ class RabbitChase extends React.Component {
                 rabbit: {
                     sound: "REEEEEEEEEEEEEEEEEE",
                     name: "Rabbit",
-                    rotX: 0.0,
-                    rotY: 0.0,
-                    rotZ: 0.0,
-                    posX: -1.0,
-                    posY: -0.7,
-                    posZ: 0.0,
+                    rotX: new Animated.Value(0),
+                    rotY: new Animated.Value(0),
+                    rotZ: new Animated.Value(0),
+                    posX: new Animated.Value(-1.0),
+                    posY: new Animated.Value(-0.7),
+                    posZ: new Animated.Value(0),
                     jumpUp: false,
                 },
                 fox: {
                     sound: "bork",
                     name: "Fox",
-                    rotX: 0.0,
-                    rotY: 0.0,
-                    rotZ: 0.0,
-                    posX: 1.0,
-                    posY: -1.0,
-                    posZ: 0.0,
+                    rotX: new Animated.Value(0),
+                    rotY: new Animated.Value(0),
+                    rotZ: new Animated.Value(0),
+                    posX: new Animated.Value(1.0),
+                    posY: new Animated.Value(-1.0),
+                    posZ: new Animated.Value(0),
                     jumpUp: true,
                 }
             }
         }
     }
 
-    walk(){
-        let fox = this.state.creatures.fox;
-        let rabbit = this.state.creatures.rabbit;
-        fox.rotY = this.state.creatures.fox.rotY - 2;
-        rabbit.rotY = this.state.creatures.rabbit.rotY - 2;
-        this.setState({fox, rabbit});
-    }
-
-    bounce(creature, boundIntensity){
-        if(creature.jumpUp){
-            creature.posY = creature.posY + 0.01;
-            creature.rotX = creature.rotX - boundIntensity;
-            this.setState({creature});
-            if(creature.posY >= -0.7){
-                creature.jumpUp = false;
-                this.setState({creature});
-            }
-        }else{
-            creature.posY = creature.posY - 0.01;
-            creature.rotX = creature.rotX + boundIntensity;
-            this.setState({creature});
-            if(creature.posY <= -1){
-                creature.jumpUp = true;
-                this.setState({creature});
-            }
-        }
-    }
-
+    /*START ANIMATIONS*/
 
     componentDidMount(){
-        this.walkInterval = setInterval(() => this.walk(), 10);
-        let fox = this.state.creatures.fox;
-        let rabbit = this.state.creatures.rabbit;
-        this.foxBounceInterval = setInterval(() => this.bounce(fox, 0.25), 10);
-        this.rabbitBounceInterval = setInterval(() => this.bounce(rabbit, 1), 10);
+        this.foxRun();
+        this.foxBounce();
+        this.rabbitRun();
+        this.rabbitBounce();
     }
 
-    componentWillUnmount() {
-        clearInterval(this.walkInterval);
-        clearInterval(this.foxBounceInterval);
-        clearInterval(this.rabbitBounceInterval);
-    }
+    /*ANIMATIONS*/
+
+    foxRun = () => {
+        let fox = this.state.creatures.fox;
+        fox.rotY.setValue(0);
+        Animated.timing(
+            fox.rotY,
+            {
+                toValue: -360,
+                duration: 4000, //in millis
+            }
+        ).start(this.foxRun);
+    };
+
+    rabbitRun = () => {
+        let rabbit = this.state.creatures.rabbit;
+        rabbit.rotY.setValue(0);
+        Animated.timing(
+            rabbit.rotY,
+            {
+                toValue: -360,
+                duration: 4000, //in millis
+            }
+        ).start(this.rabbitRun);
+    };
+
+    foxBounce = () => {
+        let fox = this.state.creatures.fox;
+        Animated.sequence([
+            //UP
+            Animated.parallel([
+                Animated.timing(
+                    fox.posY,
+                    {
+                        toValue: -0.9,
+                        duration: 500,
+                    }
+                ),
+                Animated.timing(
+                    fox.rotX,
+                    {
+                        toValue: -15,
+                        duration: 500,
+                    }
+                )
+            ]),
+            //DOWN
+            Animated.parallel([
+                Animated.timing(
+                    fox.posY,
+                    {
+                        toValue: -1,
+                        duration: 500,
+                    }
+                ),
+                Animated.timing(
+                    fox.rotX,
+                    {
+                        toValue: 0,
+                        duration: 500,
+                    }
+                )
+            ])
+        ]).start(this.foxBounce);
+    };
+
+    rabbitBounce = () => {
+        let rabbit = this.state.creatures.rabbit;
+        Animated.sequence([
+            //UP
+            Animated.parallel([
+                Animated.timing(
+                    rabbit.posY,
+                    {
+                        toValue: -0.8,
+                        duration: 500,
+                    }
+                ),
+                Animated.timing(
+                    rabbit.rotX,
+                    {
+                        toValue: 20,
+                        duration: 500,
+                    }
+                )
+            ]),
+            //DOWN
+            Animated.parallel([
+                Animated.timing(
+                    rabbit.posY,
+                    {
+                        toValue: -1,
+                        duration: 500,
+                    }
+                ),
+                Animated.timing(
+                    rabbit.rotX,
+                    {
+                        toValue: 0,
+                        duration: 500,
+                    }
+                )
+            ])
+        ]).start(this.rabbitBounce);
+    };
+
+    /*RENDER SCENE*/
 
     render() {
         let creatures = this.state.creatures;
         return (
             <View>
                 <Pano source={asset('outside.jpg')}/>
-                <Model style={{
+                <AnimatedModel style={{
                     transform: [
                         {rotateY: creatures.fox.rotY},
                         {rotateZ: creatures.fox.rotZ},
@@ -144,7 +223,7 @@ class RabbitChase extends React.Component {
                         {scale: 0.005}]}}
                        source={{obj: asset('./beasts/fox.obj'), mtl:asset('./beasts/fox.mtl')}}
                 />
-                <Model style={{
+                <AnimatedModel style={{
                     transform: [
                         {rotateY: creatures.rabbit.rotY},
                         {rotateZ: creatures.rabbit.rotZ},
